20d411613ce7057563fdef857e4e7609b12a254e
ericliudx <ericdongxuliu@gmail.com>
2025-08-13 02:25:25 -0400

    fixed some routing

--- app/src/App.js
+++ app/src/App.js
@@ -29,7 +29,6 @@ function App() {
     //   </BrowserRouter>
 
     <div className="App">
-      <h1>App is loading...</h1>
       <Routes>
         <Route path="/" element={<div>Hello from HomePage!</div>} />
         <Route path="/food-page" element={<FoodPage />} />
--- app/src/navigation/header/header.js
+++ app/src/navigation/header/header.js
@@ -1,64 +1,64 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./header.css";
 import logo1 from "../../images/logos/logo2.jpg";
 import HeaderNavigator from "./header-nav.js";
+import { Link } from "react-router-dom";
 
-function Header({title, text, picture}) {
+function Header({ title, text, picture }) {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [showXmark, setShowXmark] = useState(false);
 
-    const [menuVisible, setMenuVisible] = useState(false);
-    const [showXmark, setShowXmark] = useState(false);
+  useEffect(() => {
+    let timeoutId;
+    if (menuVisible) {
+      timeoutId = setTimeout(() => {
+        setShowXmark(true);
+      }, 700); // Adjust the timeout duration (in milliseconds) as needed
+    } else {
+      setShowXmark(false);
+    }
 
-    useEffect(() => {
-        let timeoutId;
-        if (menuVisible) {
-            timeoutId = setTimeout(() => {
-                setShowXmark(true);
-            }, 700); // Adjust the timeout duration (in milliseconds) as needed
-        } else {
-            setShowXmark(false);
-        }
-        
-        return () => clearTimeout(timeoutId); // Clean up the timeout if the component unmounts or menuVisible changes
-    }, [menuVisible]);
+    return () => clearTimeout(timeoutId); // Clean up the timeout if the component unmounts or menuVisible changes
+  }, [menuVisible]);
 
-    return (
-        <header>
-            <div className="header">
+  return (
+    <header>
+      <div className="header">
+        <div className="logo">
+          <img src={logo1} alt="" />
+        </div>
 
-                <div className="logo">
-                    <img src={logo1} alt=""/>
-                </div>
-
-                <div className="right_side">
-                    <div className="nav_bar" style={{ opacity: menuVisible ? 0 : 1 }}>
-                        <li>
-                            <a className="nav_bar_link" href="/food-page">Online Menu</a>
-                        </li>
-                        <li>
-                            <a className="nav_bar_link" href="#">Reservations</a>
-                        </li>
-                    </div>
-                    <div className="bar-container">
-                        <i 
-                            className="fa-solid fa-bars headerbar-bars" 
-                            style={{ opacity: menuVisible ? 0 : 1 }}
-                            onClick={() => setMenuVisible(true)}
-                            ></i>
-                        <i 
-                            className="fa-solid fa-xmark headerbar-x" 
-                            id="hdcross"
-                            style={{ display: showXmark ? 'block' : 'none' }}
-                            onClick={() => setMenuVisible(false)}>
-
-                        </i>
-                    </div>
-                    <HeaderNavigator menuVisible={menuVisible} />
-
-                </div>
-            </div>
-        
-        </header>
-    );
+        <div className="right_side">
+          <div className="nav_bar" style={{ opacity: menuVisible ? 0 : 1 }}>
+            <li>
+              <Link className="nav_bar_link" to="/food-page">
+                Online Menu
+              </Link>{" "}
+            </li>
+            <li>
+              <a className="nav_bar_link" href="#">
+                Reservations
+              </a>
+            </li>
+          </div>
+          <div className="bar-container">
+            <i
+              className="fa-solid fa-bars headerbar-bars"
+              style={{ opacity: menuVisible ? 0 : 1 }}
+              onClick={() => setMenuVisible(true)}
+            ></i>
+            <i
+              className="fa-solid fa-xmark headerbar-x"
+              id="hdcross"
+              style={{ display: showXmark ? "block" : "none" }}
+              onClick={() => setMenuVisible(false)}
+            ></i>
+          </div>
+          <HeaderNavigator menuVisible={menuVisible} />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
